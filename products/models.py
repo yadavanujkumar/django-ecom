135c354777972a15a79922a9f6076bc67da7f06b
yadavanujkumar <thunderstruck9999@gmail.com>
2023-08-16 17:07:42 +0530

    change in products dynamic pricing

--- products/models.py
+++ products/models.py
@@ -55,10 +55,12 @@ class Product(BaseModel):
     def __str__(self) -> str:
         return self.product_name
 
-
+    def gert_product_price_by_size(self, size):
+        return self.price + SizeVariant.objects.get(size_name=size).price
 
 
 
 class ProductImage(BaseModel):
     product = models.ForeignKey(Product , on_delete=models.CASCADE , related_name="product_images")
     image =  models.ImageField(upload_to="product")
+
--- products/views.py
+++ products/views.py
@@ -9,7 +9,15 @@ from products.models import Product
 def get_product(request , slug):
    # try:
         product = Product.objects.get(slug =slug)
-        return render(request  , 'product/product.html' , context = {'product' : product})
+        context = {'product ': product}
+        if request.GET.get('size'):
+            size = request.GET.get('size')
+            price = product.gert_product_price_by_size(size)
+            context['selected_size'] = size
+            context['update_price'] = price
+            print(price)
+             
+        return render(request  , 'product/product.html' , context = context )
 
   #  except Exception as e:
  #       print(e)
