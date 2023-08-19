d50a156379f85db285ff5b58dc743e7130aced90
yadavanujkumar <thunderstruck9999@gmail.com>
2023-08-19 09:51:21 +0530

    dynamic price

--- products/views.py
+++ products/views.py
@@ -9,7 +9,7 @@ from products.models import Product
 def get_product(request , slug):
    # try:
         product = Product.objects.get(slug =slug)
-        context = {'product ': product}
+        context = {'product':product}
         if request.GET.get('size'):
             size = request.GET.get('size')
             price = product.gert_product_price_by_size(size)
