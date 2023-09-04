2166cacfe57373de97a54e636ff42057c92da541
yadavanujkumar <thunderstruck9999@gmail.com>
2023-09-04 10:23:25 +0530

    models,view

--- products/views.py
+++ products/views.py
@@ -7,15 +7,16 @@ from django.http import HttpResponseRedirect
 
 
 def get_product(request , slug):
-   # try:
+
+    try:
         product = Product.objects.get(slug =slug)
         context = {'product':product}
         if request.GET.get('size'):
             size = request.GET.get('size')
-            price = product.gert_product_price_by_size(size)
+            price = product.get_product_price_by_size(size)
             context['selected_size'] = size
-            context['update_price'] = price
-            print(price)
+        context['update_price'] = price
+        print(price)
              
         return render(request  , 'product/product.html' , context = context )
 
@@ -24,7 +25,7 @@ def get_product(request , slug):
 def add_to_cart(request, uid):
     variant = request.GET.get('variant') 
 
-    product =  Product.objects.get(uid= uid)
+    product =  Product.objects.get(uid =uid)
     user = request.user
     cart , _ = Cart.objects.get_or_create(user = user , is_paid = False)
     cart_item = CartItems.objects.create(cart = cart , product = product )
