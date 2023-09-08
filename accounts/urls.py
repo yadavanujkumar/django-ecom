c0d6525baec96d43fde49fe56e9143a91bc0658d
yadavanujkumar <thunderstruck9999@gmail.com>
2023-09-08 21:54:44 +0530

    cart

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from accounts.views import login_page,register_page , activate_email ,add_to_cart , remove_cart
+from accounts.views import login_page,register_page , activate_email ,add_to_cart , remove_cart, cart
 #from products.views import 
-from accounts.models import Cart
+
 
 urlpatterns = [
    path('login/' , login_page , name="login" ),
    path('register/' , register_page , name="register"),
    path('activate/<email_token>/' , activate_email , name="activate_email"),
-   path('cart/' , Cart , name="cart"),
+   path('cart/' , cart , name="cart"),
    path('add-to-cart/<uid>/' , add_to_cart, name = "add_to_cart"),
    path('remove-cart/<cart_item_uid>' , remove_cart , name = "remove_cart")
 ]
