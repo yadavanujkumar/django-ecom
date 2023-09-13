4544cfe5d74836497bb4747e13c43c68ef4fb28f
yadavanujkumar <thunderstruck9999@gmail.com>
2023-09-14 00:03:38 +0530

    somethings

--- accounts/views.py
+++ accounts/views.py
@@ -11,6 +11,7 @@ from django.core.exceptions import ObjectDoesNotExist
 import razorpay
 from products.models import *
 # Create your views here.
+from ecom.settings import *
 
 
 def login_page(request):
@@ -55,7 +56,7 @@ def logout(request, *args, **kwargs):
 
 def about_us(request):
     # Your view logic here
-    return render(request, 'about_us.html')
+    return render(request ,'accounts/about_us.html')
 
 
 def register_page(request):
@@ -145,51 +146,47 @@ def cart(request):
     user_cart = None
     try:
         user_cart = Cart.objects.get(is_paid=False, user=request.user)
-    except Exception as e:
-        # If no cart matching the conditions is found, create an empty cart.
-        print(e)
-    
+    except Cart.DoesNotExist:
+        pass # If no cart matching the conditions is found, create an empty cart.
+          # No need to print anything here; just pass
+
     cart_total = user_cart.get_cart_total() if user_cart else 0  # Calculate the total or set it to 0 if cart is None
+
     if request.method == 'POST':
-        coupon =  request.POST.get('coupon')
+        coupon = request.POST.get('coupon')
         coupon_obj = Coupon.objects.filter(coupon_code__icontains=coupon)
-        
+
         if not coupon_obj.exists():
             messages.warning(request, 'Invalid Coupon brooo')
             return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
-        if user_cart.coupon:
-            messages.warning(request, 'Coupon already exists brooooo ')
+        if user_cart and user_cart.coupon:
+            messages.warning(request, 'Coupon already exists brooooo')
             return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
-        if user_cart.get_cart_total() > coupon_obj[0].minimum_amount:
-             messages.warning(request, f'Amount should be greater than  {coupon_obj[0].minimum_amount}')
-             return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
-
-        if coupon_obj[0].is_expired:
-            messages.warning(request, 'Coupon experied ')
+        if user_cart and user_cart.get_cart_total() > coupon_obj[0].minimum_amount:
+            messages.warning(request, f'Amount should be greater than {coupon_obj[0].minimum_amount}')
             return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
+        if user_cart and coupon_obj[0].is_expired:
+            messages.warning(request, 'Coupon expired')
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
+        if user_cart:
+            user_cart.coupon = coupon_obj[0]
+            user_cart.save()
 
-        user_cart.coupon = coupon_obj[0]
-        user_cart.save() 
+            messages.success(request, 'Coupon used')
+            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
-        messages.success(request, 'Coupon used ')
-        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
     if user_cart:
-        client = razorpay.Client(auth=(settings.KEY , settings.SECERET))
-        payment = client.order.create({'amount' : user_cart.get_cart_total() *100, 'currency' : 'INR' , 'payment_capture' : 1 })
+        client = razorpay.Client(auth=(settings.KEY, settings.SECERET))
+        payment = client.order.create({'amount': user_cart.get_cart_total() * 100, 'currency': 'INR', 'payment_capture': 1})
 
         user_cart.razor_pay_order_id = payment['id']
         user_cart.save()
-        print("*****")
-        print(payment)
-        print("****")
 
-   # payment = None
-
-    context = {'user_cart': user_cart, 'payment' : payment}
+    context = {'user_cart': user_cart, 'payment': payment if user_cart else None}
     return render(request, 'accounts/cart.html', context)
 
 def remove_coupon(request, cart_id):
