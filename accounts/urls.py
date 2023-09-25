a137cebac22ddec15800cc9c10cbd402234c7227
yadavanujkumar <thunderstruck9999@gmail.com>
2023-09-25 10:39:08 +0530

    pass reset

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from accounts.views import *
 #from products.views import 
+from django.contrib.auth import views as auth_views
 
 
 urlpatterns = [
@@ -17,6 +18,9 @@ urlpatterns = [
    path('user-details/', user_details, name='user_details'),
    path('order_history/', order_history, name='order_history'),
    path('contact-us/', contact_us, name='contact_us'),
-
+   path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+   path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+   path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+   path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
 ]
